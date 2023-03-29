ff6bc900bd51bb3790b49d81447c7f7211a09ec2
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-03-29 20:03:48 +0000

    Given two cities, order them alphabetically

---
+++ src/chapter4/OrderTwoCities.java
@@ -0,0 +1,25 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class OrderTwoCities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the first city : ");
+		String firstCity = input.nextLine();
+		
+		System.out.print("Enter the second city : ");
+		String secondCity = input.nextLine();
+		
+		if(firstCity.compareTo(secondCity) <= 0) 
+			System.out.println("The cities in alphabetical order are : " + firstCity + " " + secondCity );
+		else
+			System.out.println("The cities in alphabetical order are : " + secondCity + " " + firstCity  );
+		
+		
+	}
+
+}
