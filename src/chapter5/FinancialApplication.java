ea413a324dd2e0f09fb8c8c2a0737ebcdd5ced50
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-06 21:11:09 +0000

    Calculate four year cost of tuition cost

---
+++ src/chapter5/FinancialApplication.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+public class FinancialApplication {
+
+	public static void main(String[] args) {
+		// compute future tuition
+		int tuition = 10000;
+		int tuitionIncrease = 0;
+		int tuitionCost = 0;
+		
+		for(int i=1; i <= 14; i++) {
+			if(i <= 10) {
+				tuitionIncrease = tuition * 5 / 100;
+				System.out.println("Tuition increase for the year " + i +" = 5% of "+ tuition + " = " + tuitionIncrease);
+				System.out.println("Tuition for the year " + i +" = " + tuition + " + " + " tuitionIncrease " + " = " + tuition + tuitionIncrease );
+			}else {
+				tuitionCost += tuition; 
+			}
+			tuition += tuitionIncrease; 
+		}
+		
+		System.out.println("Cost of Tuition after 10 years is : " + tuitionCost);
+	}
+
+}
