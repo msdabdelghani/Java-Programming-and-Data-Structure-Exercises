c73ebb1c77cdff0697601bf8b6bb6499c7543a8a
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-07 00:43:39 +0000

    Display pyramid based on two digits

--- src/chapter5/DisplayPyramid.java
+++ src/chapter5/DisplayPyramid.java
@@ -1,10 +1,37 @@
 package chapter5;
 
+import java.util.Scanner;
+
 public class DisplayPyramid {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter two digit number: ");
+		int n = input.nextInt();
+		input.close();
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n-i ; j++)
+				System.out.print("   ");
+			int valueBefore = i + 1;
+			for(int j = n-i; j < n + 1; j++) {
+				if(valueBefore < 10)
+					System.out.print(valueBefore-- + "  ");
+				else
+					System.out.print(valueBefore-- + " ");
+			}
+			int valueAfter = 2;
+			for(int j = n + 1; j < n + i + 1; j++) {
+				if(valueBefore < 10)
+					System.out.print(valueAfter++ + "  ");
+				else
+					System.out.print(valueAfter++ + " ");
+			}
+			for(int j = n + i + 1; j < 2 * n -1; j++) 
+				System.out.print("   ");
+			System.out.println();
+		}
 	}
-
 }
+
+
