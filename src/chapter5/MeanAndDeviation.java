8033b4b4f5c0da1b5ec6a6be1664ce420a3334c6
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-12 21:18:34 +0000

    Compute mean and deviation

--- src/chapter5/MeanAndDeviation.java
+++ src/chapter5/MeanAndDeviation.java
@@ -1,1 +1,25 @@
+package chapter5;
 
+import java.util.Scanner;
+
+public class MeanAndDeviation {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int n = 10;
+		double sumOfX = 0, sumOfX2 = 0;
+		System.out.print("Enter 10 numbers: ");
+		for(int i = 1;i <= n;i++) {
+			double number =  input.nextDouble();
+			sumOfX += number;
+			sumOfX2 += number * number;
+		}
+		double mean = sumOfX / n;
+		double deviation = Math.sqrt((sumOfX2 - (sumOfX * sumOfX / n)) / (n -1));
+		System.out.println("The mean is " + mean);
+		System.out.println("The deviation is " + deviation);
+
+	}
+
+}
