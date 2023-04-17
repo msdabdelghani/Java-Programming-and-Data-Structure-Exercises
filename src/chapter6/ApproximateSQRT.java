e8ea358c2f028f2d3402fed7fbc4c867ada7858c
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-17 22:28:56 +0000

    Approximate sqrt using Babylonian method

--- src/chapter6/ApproximateSQRT.java
+++ src/chapter6/ApproximateSQRT.java
@@ -1,5 +1,23 @@
 package chapter6;
 
 public class ApproximateSQRT {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public static double sqrt(long n) {
+		long lastGuess = (long)(Math.random()* (n+1));
+		long nextGuess;
+		while(true) {
+			nextGuess = (lastGuess + n / lastGuess) / 2;
+			if(Math.abs(nextGuess - lastGuess) <= 0.0001) {
+				return nextGuess;
+			}else {
+				nextGuess = lastGuess;
+				if(Math.abs(nextGuess - lastGuess) <= 0.0001) {
+					return nextGuess;
+				}
+			}
+		}
+	}
 }
