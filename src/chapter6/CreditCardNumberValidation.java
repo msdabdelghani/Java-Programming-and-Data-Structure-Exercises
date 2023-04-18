857c790e58e41b029d842dba01bf483c3b12f0e6
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-18 22:59:25 +0000

    Credit Card Number Validation Initialization

---
+++ src/chapter6/CreditCardNumberValidation.java
@@ -0,0 +1,41 @@
+package chapter6;
+
+public class CreditCardNumberValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/** Return true if the card number is valid */
+	public static boolean isValid(long number) {
+		return true;
+	}
+	/** Get the result from Step 2 */
+	public static int sumOfDoubleEvenPlace(long number) {
+		return 1;
+	}
+	/** Return this number if it is a single digit, otherwise,
+	 return the sum of the two digits */
+	public static int getDigit(int number) {
+		return number;
+	}
+	/** Return sum of odd-place digits in number */
+	public static int sumOfOddPlace(long number) {
+		return 1;
+	}
+	/** Return true if the number d is a prefix for number */
+	public static boolean prefixMatched(long number, int d) {
+		return true;
+	}
+	/** Return the number of digits in d */
+	public static int getSize(long d) {
+		return 1;
+	}
+	/** Return the first k number of digits from number. If the
+	* number of digits in number is less than k, return number. */
+	public static long getPrefix(long number, int k) {
+		return number;
+	}
+
+}
