bf703c05d7cd09a8e4c131c7ed9bacdf21dab793
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-18 22:16:37 +0000

    Print first 100 Emirp Numbers

--- src/chapter6/Emirp.java
+++ src/chapter6/Emirp.java
@@ -2,6 +2,22 @@ package chapter6;
 
 public class Emirp {
 	
+	public static void main(String args[]) {
+		int count = 0;
+		int number = 1;
+		
+		while(count < 100) {
+			if(isEmirp(number)) {
+				System.out.print(number + " ");
+				count++;
+				if(count % 10 == 0) {
+					System.out.println();
+				}
+			}
+			number++;
+		}
+	}
+	
 	public static boolean isEmirp(int number) {
 		int reversedNumber = PalindromeInteger.reverse(number);
 		boolean isNumberPalindrome = PalindromeInteger.isPalindrome(number);
