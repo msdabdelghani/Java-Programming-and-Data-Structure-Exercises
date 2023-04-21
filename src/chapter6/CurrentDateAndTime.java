5898adf4f87e48f427d848b5570a7d63c5872ab8
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-21 23:52:58 +0000

    Current Date : get current year

--- src/chapter6/CurrentDateAndTime.java
+++ src/chapter6/CurrentDateAndTime.java
@@ -2,14 +2,33 @@ package chapter6;
 
 public class CurrentDateAndTime {
 	public static void main(String[] args) {
-		System.out.println(currentHour() + ":" + currentMinute() + ":" + currentSecond());
+		System.out.println(currentHour() + ":" + currentMinute() + ":" + currentSecond() + " " + totalDays());
 	}
 	
+	public static int currentYear() {
+		long days = 0;
+		int year = 1970;
+		while(days < totalDays()) {
+			if(isLeapYear(year)) 
+				days += 366;
+			else
+				days += 365;
+			year++;
+		}
+		return year;
+	}
+	
+	public static long totalDays() {
+		//return totalSeconds() / (24 * 60 * 60);
+		System.out.println(totalSeconds() / (24 * 60 * 60));
+		System.out.println((totalSeconds() / (24 * 60 * 60)) % 365);
+		return totalSeconds() / (24 * 60 * 60);
+	}
 	
 	
 	public static long currentHour() {
 		long totalHour = totalSeconds() / (60 * 60);
-		return totalHour % 60;
+		return totalHour % 24;
 	}
 	
 	public static long currentMinute() {
