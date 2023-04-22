1db2bca9e628f1d1aa3928601f085ca7c0ea8244
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-04-22 23:37:50 +0000

    Current Date and Time : Added a method to get current day

--- src/chapter6/CurrentDateAndTime.java
+++ src/chapter6/CurrentDateAndTime.java
@@ -2,7 +2,34 @@ package chapter6;
 
 public class CurrentDateAndTime {
 	public static void main(String[] args) {
-		System.out.println(currentHour() + ":" + currentMinute() + ":" + currentSecond() + " " + currentYear());
+		System.out.println(currentHour() + ":" + currentMinute() + ":" + currentSecond() + " " + currentYear()+ " " + currentDay());
+	}
+	
+	public static int currentDay() {
+		int year = 1970;
+		long remainingDays = totalDays();
+		while(remainingDays > 365) {
+			remainingDays -= isLeapYear(year) ? 366 : 365;
+			year++;
+		}
+		for(int month = 1; month <= 12; month++ ) {
+			int daysOfMonth = 0;
+			switch(month) {
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12 : 
+				daysOfMonth = 31;
+				break;
+			case 4: case 6: case 9: case 11:
+				daysOfMonth = 30;
+				break;
+			case 2:
+				daysOfMonth = isLeapYear(year) ? 29 : 28;
+				break;
+			}
+			if(remainingDays <= daysOfMonth) break;
+			else remainingDays -= daysOfMonth;
+		}
+		int currentDay = (int)remainingDays + 1;
+		return currentDay;
 	}
 	
 	public static int currentYear() {
