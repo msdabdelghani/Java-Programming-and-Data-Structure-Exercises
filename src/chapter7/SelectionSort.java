17583ece84fb48e24e876ba46346697e4aa8297d
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2023-05-07 22:05:18 +0100

    Binary Search in Array : Using the min Index

--- src/chapter7/SelectionSort.java
+++ src/chapter7/SelectionSort.java
@@ -15,17 +15,17 @@ public class SelectionSort {
 	public static int[] selectionSort(int[] array) {
 		for(int  i = 0; i < array.length - 1; i++ )	{
 			int min = array[i];
-			int index = -1;
+			int minIndex = i;
 			for(int j = i + 1; j < array.length; j++ ) {
 				if(array[j] < min) {
 					min = array[j];	
-					index = j;
+					minIndex = j;
 				}
 			}
-			if(index != -1) {
+			if(minIndex != i) {
 				int temp = array[i];
 				array[i] = min;
-				array[index] = temp;
+				array[minIndex] = temp;
 			}
 		}
 		return array;
